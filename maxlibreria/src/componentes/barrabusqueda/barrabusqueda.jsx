227868d8118184dd0b14993d8cdcecb464920b6c
Return0Zero <69299788+Return0Zero@users.noreply.github.com>
2022-03-21 22:02:30 -0400

    generos

---
+++ maxlibreria/src/componentes/barrabusqueda/barrabusqueda.jsx
@@ -0,0 +1,45 @@
+import React, { useState} from 'react';
+import './barrabusqueda.css';
+import Lupa from '../../imagenes/iconos/lupa.png'
+
+const BarraBusqueda = ({ placeholder, data}) => {
+
+    const [dataFiltrada, colocarDataFiltrada] =useState([])
+    const obtenerFiltro =(event) => {
+        const palabraBuscada = event.target.value
+        const nuevoFiltro = data.filter((value) => {
+            return value.titulo.toLowerCase().includes(palabraBuscada.toLowerCase());
+        });
+
+        if (palabraBuscada === "") {
+            colocarDataFiltrada([]);
+        } else {
+            colocarDataFiltrada(nuevoFiltro)
+        }
+        colocarDataFiltrada(nuevoFiltro);
+    };
+    
+    return (
+        <div className='barrabusqueda-seccion'>
+            <div className='seccion-inputBusqueda'>
+                <input type="text" placeholder={placeholder} className="inputBusqueda" onChange={obtenerFiltro} />
+                <div className='contenedor--icono-busqueda'> <img src={Lupa} alt="lupa" className='icono-busqueda' /> </div>
+            </div>
+            {dataFiltrada.length != 0 && (
+            <div className='resultadoData'>
+                {dataFiltrada.map((value, key) => {
+                    return (
+                        <div className='dataLibros'>
+                            <p>{value.titulo}</p>
+                        </div>
+                    
+                    
+                    );
+                })}
+            </div>
+            )}
+            </div>
+    )
+}
+
+export default BarraBusqueda;
--- maxlibreria/src/contenedores/coleccion/coleccion.jsx
+++ maxlibreria/src/contenedores/coleccion/coleccion.jsx
@@ -1,10 +1,55 @@
 import React from 'react';
 import './coleccion.css';
+import BarraBusqueda from '../../componentes/barrabusqueda/barrabusqueda';
+// import DataLibros from 'archivo para conectar la data';
+import Linea from '../../imagenes/iconos/linea.svg'
+import Libro from '../../componentes/libro/libro'
 
 const Coleccion = () => {
     return (
         <div className='header-section'>
             <h1 className='coleccion-titulo'>NUESTRA COLECCIÓN</h1>
+            <BarraBusqueda placeholder="Titulo, Autor, Género y más" /* ----- data={DataLibros} ----- */ ></BarraBusqueda>
+            <div className='generosLibros'>
+                <div className='generoDidactico'>
+                    <div className='genero-titulo'>
+                        <h2>Género Didáctico</h2>
+                        <img src={Linea} alt="Linea Decorativa" className='lineaDecorativa' />
+                    </div>
+                    <div className='coleccion--libro-contenedor'>
+                        <Libro></Libro>
+                        <Libro></Libro>
+                        <Libro></Libro>
+                        <Libro></Libro>
+                    </div>
+                </div>
+                <div className='generoDramatico'>
+                    <div className='genero-titulo'>
+                        <h2>Género Dramático</h2>
+                        <img src={Linea} alt="Linea Decorativa" className='lineaDecorativa' />
+                    </div>
+                    <div className='coleccion--libro-contenedor'>
+                        <Libro></Libro>
+                        <Libro></Libro>
+                        <Libro></Libro>
+                        <Libro></Libro>
+                    </div>
+
+                </div>
+                <div className='generoNarrativo'>
+                    <div className='genero-titulo'>
+                        <h2>Género Narrativo</h2>
+                        <img src={Linea} alt="Linea Decorativa" className='lineaDecorativa' />
+                    </div>
+                    <div className='coleccion--libro-contenedor'>
+                        <Libro></Libro>
+                        <Libro></Libro>
+                        <Libro></Libro>
+                        <Libro></Libro>
+                    </div>
+
+                </div>
+            </div>
         </div>
     )
 }
