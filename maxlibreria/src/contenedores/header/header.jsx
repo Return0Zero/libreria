288804299bcce286916a629c7932a65132c96968
Leopoldo José Cálix Parada <leopoldoj.calix@gmail.com>
2022-03-26 09:43:25 -0600

    Responsive matter

--- maxlibreria/src/contenedores/header/header.jsx
+++ maxlibreria/src/contenedores/header/header.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './header.css';
 import ImagenFondo from '../../imagenes/imagen-fondo.png';
+import ImagenFondoMovil from '../../imagenes/imagen-fondo-700px.png'
 
 const Header = () => {
     return (
         <div className='header-seccion'>
-            <img src={ImagenFondo} alt='imagen de fondo' className='imagen-fondo'/>
+            <img src={ImagenFondo} alt='imagen de fondo' className='imagen-fondo' />
+            <img src={ImagenFondoMovil} alt='imagen de fondo movil' className='imagen-fondo-movil' />
             <p className='header--titulo'>¡Libros! Los libros son el regalo más grande que una persona puede dar a otra.
             </p>
             <div className='CTA--contenedor'>
