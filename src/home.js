36318f12f78bb0ef8ab63133ed1f2cd4c5aa0e95
Return0Zero <69299788+Return0Zero@users.noreply.github.com>
2022-03-29 19:44:23 -0400

    .

--- src/home.js
+++ src/home.js
@@ -5,14 +5,15 @@ import { Coleccion, Maxlibreria, Header, Navbar, Footer, Recomendados } from './
 
 function Home() {
   
+  
   return (
     <div className="">
-    <Navbar></Navbar>
-    <Header></Header>
-    <Recomendados></Recomendados>
-    <Coleccion></Coleccion>
-    <Maxlibreria></Maxlibreria>
-    <Footer></Footer>
+      <Navbar></Navbar>
+      <Header></Header>
+      <Recomendados></Recomendados>
+      <Coleccion></Coleccion>
+      <Maxlibreria></Maxlibreria>
+      <Footer></Footer>
     </div>
   );
 }
