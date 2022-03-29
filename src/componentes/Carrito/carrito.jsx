6fb9ad49afd057d8a6492213f7a7d2c2417b988b
kevcaleb <kevcaleb1@gmail.com>
2022-03-29 09:31:59 -0700

    Carrito 3 Comando

--- src/componentes/Carrito/carrito.jsx
+++ src/componentes/Carrito/carrito.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import './compras.css';
+import './carrito.css';
 import Portada1 from '../../imagenes/libros/don-quijote-de-la-mancha.jpg'
 
-const Compras = () => {
+const Carrito = () => {
     return (
-        <div className='componente-compras'>
-            <h1>Compras</h1>
+        <div className='componente-carrito'>
+               <NuevoComponente></NuevoComponente>
+
+            <h1>Carrito</h1>
         </div>
     )
 }
 
-export default Compras;
+export default Carrito;
