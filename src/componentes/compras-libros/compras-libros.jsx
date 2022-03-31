59ed2f1ec5e943f842b390fc4ec908dbafbc17de
kevcaleb1 <kevcaleb1@gmail.com>
2022-03-30 19:06:00 -0600

    Carrito sigui en el trabajo

--- src/componentes/compras-libros/compras-libros.jsx
+++ src/componentes/compras-libros/compras-libros.jsx
@@ -1,12 +1,14 @@
 import React from 'react';//Improtar parte de la sintaxis de react
 import './compras-libros.css';
+import Principito from '../../imagenes/libros/el-principito.jpg'
 
-
-const Compras = () => //Programacion funcional de react en lugar de programacion orientada a objetos, por eso utilizamos esta constante para poder utilizar lo que aqui se establece
+const Compraslibros = () => //Programacion funcional de react en lugar de programacion orientada a objetos, por eso utilizamos esta constante para poder utilizar lo que aqui se establece
 {
     return ( //jsx es una extension de react de la sintaxis de lenguaje de javascript, herramienta para estructurar componentes, el return regresa el jsx que termina siendo la version de html de react.
         <div className='componente-compras'>
+            <h1>Carrito de prueba</h1>
+            <img src= {Principito} alt='Foto de portada' className='foto-principito'/>
         </div>
     )
 }
-export default Compras;
+export default Compraslibros;
--- src/componentes/compras/compras.jsx
+++ src/componentes/compras/compras.jsx
@@ -1,18 +1,14 @@
 import React from 'react';
 import './compras.css';
 import Principito from '../../imagenes/libros/el-principito.jpg'
+import ComprasLibros from '../compras-libros/compras-libros'
+
 
 const Compras = () => 
 {
     return (
         <div className='componente-compras'>
-            <h1>Compras</h1>
-            <img src={Principito} alt='Foto de libro' className='foto-principito'/>
-            <div><p>Hola</p></div> 
-            <img src={Principito} alt='Foto de libro' className='foto-principito'/>
-            <p>Hola</p>
-            <img src={Principito} alt='Foto de libro' className='foto-principito'/>
-            <p>Hola</p>
+            <ComprasLibros></ComprasLibros>
         </div>
     )
 }
